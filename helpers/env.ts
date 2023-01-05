61b4774fe82c51be6dc041e5f6df4b870d1e27a4
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-01-05 09:31:09 +0530

    removed console log

--- helpers/env.ts
+++ helpers/env.ts
@@ -1,19 +1,18 @@
-export const getEnv = (): 'dev' | 'production' | 'local' => {
-  const envVar = process.env.NEXT_PUBLIC_XMTP_ENVIRONMENT
-  if (envVar === 'production') {
-    return envVar
+export const getEnv = (): "dev" | "production" | "local" => {
+  const envVar = process.env.NEXT_PUBLIC_XMTP_ENVIRONMENT;
+  if (envVar === "production") {
+    return envVar;
   }
-  if (envVar === 'local') {
-    return envVar
+  if (envVar === "local") {
+    return envVar;
   }
-  return 'dev'
-}
+  return "dev";
+};
 
 export const isAppEnvDemo = (): boolean => {
-  console.log(process.env.NEXT_PUBLIC_APP_ENVIRONMENT)
-  return process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'demo'
-}
+  return process.env.NEXT_PUBLIC_APP_ENVIRONMENT === "demo";
+};
 
 export const tagStr = (): string | null => {
-  return getEnv() === 'production' ? null : getEnv().toLocaleUpperCase()
-}
+  return getEnv() === "production" ? null : getEnv().toLocaleUpperCase();
+};
