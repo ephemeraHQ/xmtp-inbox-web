4e4fdc4d2c86d5965caaf350d5ae34b4847805a3
daria-github <daria@xmtp.com>
2023-02-10 10:57:59 -0800

    updated test retries

--- cypress/e2e/connect_disconnect.cy.ts
+++ cypress/e2e/connect_disconnect.cy.ts
@@ -6,61 +6,70 @@ import {
   connectDisconnectFlow
 } from '../test_utils';
 
-describe('Connected Test Cases', () => {
-  beforeEach(() => {
-    startDemoEnv();
-    // In connected flow, empty message should render before any tests run
-    checkElement('empty-message-header');
-  });
+describe(
+  'Connected Test Cases',
+  {
+    retries: {
+      runMode: 2,
+      openMode: 1
+    }
+  },
+  () => {
+    beforeEach(() => {
+      startDemoEnv();
+      // In connected flow, empty message should render before any tests run
+      checkElement('empty-message-header');
+    });
 
-  it('Shows expected left panel fields when logged in with a connected wallet and no existing messages', () => {
-    const elements = [
-      'xmtp-logo',
-      'new-message-cta',
-      'empty-message-icon',
-      'empty-message-header',
-      'empty-message-subheader',
-      'connected-footer-primary-text',
-      'connected-footer-secondary-text',
-      'connected-footer-image',
-      'settings-icon'
-    ];
+    it('Shows expected left panel fields when logged in with a connected wallet and no existing messages', () => {
+      const elements = [
+        'xmtp-logo',
+        'new-message-cta',
+        'empty-message-icon',
+        'empty-message-header',
+        'empty-message-subheader',
+        'connected-footer-primary-text',
+        'connected-footer-secondary-text',
+        'connected-footer-image',
+        'settings-icon'
+      ];
 
-    elements.forEach((element) => checkElement(element));
-  });
+      elements.forEach((element) => checkElement(element));
+    });
 
-  it('Shows expected right panel fields when logged in with a connected wallet and no existing messages', () => {
-    checkElement('message-to-key');
-    checkElement('message-to-subtext');
-  });
+    it('Shows expected right panel fields when logged in with a connected wallet and no existing messages', () => {
+      checkElement('message-to-key');
+      checkElement('message-to-subtext');
+    });
 
-  it('Shows expected fields when clicking on settings icon while connected', () => {
-    cy.get(`[data-testid="settings-icon"]`).click();
+    it('Shows expected fields when clicking on settings icon while connected', () => {
+      cy.get(`[data-testid="settings-icon"]`).click();
 
-    const elements = ['xmtp-version', 'copy-address-cta', 'disconnect-wallet-cta'];
-    elements.forEach((element) => checkElement(element));
+      const elements = ['xmtp-version', 'copy-address-cta', 'disconnect-wallet-cta'];
+      elements.forEach((element) => checkElement(element));
 
-    cy.get(`[data-testid="disconnect-wallet-cta"]`).click();
-    checkElement('no-wallet-connected-header');
-  });
+      cy.get(`[data-testid="disconnect-wallet-cta"]`).click();
+      checkElement('no-wallet-connected-header');
+    });
 
-  it('Can reconnect to wallet from connect button', () => {
-    disconnectWallet();
-    connectDisconnectFlow('no-wallet-connected-cta');
-  });
-  it('Can reconnect to wallet from sign in with wallet text', () => {
-    disconnectWallet();
-    connectDisconnectFlow('no-wallet-connected-footer-secondary-text');
-  });
-  it('Can reconnect to wallet from settings icon', () => {
-    disconnectWallet();
-    connectDisconnectFlow('settings-icon');
-  });
-  it('Can reconnect to wallet from connect section link', () => {
-    disconnectWallet();
-    connectDisconnectFlow('connect-section-link');
-  });
-});
+    it('Can reconnect to wallet from connect button', () => {
+      disconnectWallet();
+      connectDisconnectFlow('no-wallet-connected-cta');
+    });
+    it('Can reconnect to wallet from sign in with wallet text', () => {
+      disconnectWallet();
+      connectDisconnectFlow('no-wallet-connected-footer-secondary-text');
+    });
+    it('Can reconnect to wallet from settings icon', () => {
+      disconnectWallet();
+      connectDisconnectFlow('settings-icon');
+    });
+    it('Can reconnect to wallet from connect section link', () => {
+      disconnectWallet();
+      connectDisconnectFlow('connect-section-link');
+    });
+  }
+);
 
 describe('Disconnected Test Cases', () => {
   beforeEach(() => {
--- cypress/e2e/messaging.cy.ts
+++ cypress/e2e/messaging.cy.ts
@@ -1,47 +1,56 @@
 import { startDemoEnv, sendAndEnterMessage, checkElement, checkMissingElement } from '../test_utils';
 
-describe('Messaging Test Cases', () => {
-  beforeEach(() => {
-    startDemoEnv();
-    // In connected flow, empty message should render before any tests run
-    checkElement('empty-message-header');
-  });
-  const testUserWithXmtpAccount = '0x78BfD39428C32Be149892d64bEE6C6f90aedEec1';
+describe(
+  'Messaging Test Cases',
+  {
+    retries: {
+      runMode: 2,
+      openMode: 1
+    }
+  },
+  () => {
+    beforeEach(() => {
+      startDemoEnv();
+      // In connected flow, empty message should render before any tests run
+      checkElement('empty-message-header');
+    });
+    const testUserWithXmtpAccount = '0x78BfD39428C32Be149892d64bEE6C6f90aedEec1';
 
-  const shortMessage = 'hello';
-  const longMessage =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et ultrices neque ornare aenean euismod elementum. Etiam tempor orci eu lobortis elementum nibh. Tincidunt ornare massa eget egestas purus. Purus sit amet volutpat consequat mauris. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Neque ornare aenean euismod elementum nisi quis eleifend. Amet consectetur adipiscing elit pellentesque habitant morbi. Malesuada fames ac turpis egestas sed tempus urna. Morbi quis commodo odio aenean sed adipiscing diam donec. Id aliquet risus feugiat in. Massa sed elementum tempus egestas sed. Lectus mauris ultrices eros in cursus turpis massa tincidunt. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Cursus eget nunc scelerisque viverra mauris in aliquam. Enim sed faucibus turpis in eu mi bibendum neque egestas. Lectus urna duis convallis convallis tellus id. Vitae semper quis lectus nulla at.';
+    const shortMessage = 'hello';
+    const longMessage =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et ultrices neque ornare aenean euismod elementum. Etiam tempor orci eu lobortis elementum nibh. Tincidunt ornare massa eget egestas purus. Purus sit amet volutpat consequat mauris. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Neque ornare aenean euismod elementum nisi quis eleifend. Amet consectetur adipiscing elit pellentesque habitant morbi. Malesuada fames ac turpis egestas sed tempus urna. Morbi quis commodo odio aenean sed adipiscing diam donec. Id aliquet risus feugiat in. Massa sed elementum tempus egestas sed. Lectus mauris ultrices eros in cursus turpis massa tincidunt. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Cursus eget nunc scelerisque viverra mauris in aliquam. Enim sed faucibus turpis in eu mi bibendum neque egestas. Lectus urna duis convallis convallis tellus id. Vitae semper quis lectus nulla at.';
 
-  it('Can send a new message to user in XMTP network', () => {
-    sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 1);
-  });
+    it('Can send a new message to user in XMTP network', () => {
+      sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 1);
+    });
 
-  it('Can send a long message to user in XMTP network', () => {
-    sendAndEnterMessage(testUserWithXmtpAccount, longMessage, 1);
-  });
+    it('Can send a long message to user in XMTP network', () => {
+      sendAndEnterMessage(testUserWithXmtpAccount, longMessage, 1);
+    });
 
-  it('Can send multiple messages to user in XMTP network', () => {
-    sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 6);
-  });
+    it('Can send multiple messages to user in XMTP network', () => {
+      sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 6);
+    });
 
-  it('Displays messages in the correct order', () => {
-    sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 6, true);
-  });
+    it('Displays messages in the correct order', () => {
+      sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 6, true);
+    });
 
-  it('Shows correct view when editing address', () => {
-    sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 1);
-    checkElement('message-to-input').click().type('{backspace}');
-    checkMissingElement('message-input');
-    checkElement('message-to-input').type('1');
-    checkElement('message-input');
-  });
+    it('Shows correct view when editing address', () => {
+      sendAndEnterMessage(testUserWithXmtpAccount, shortMessage, 1);
+      checkElement('message-to-input').click().type('{backspace}');
+      checkMissingElement('message-input');
+      checkElement('message-to-input').type('1');
+      checkElement('message-input');
+    });
 
-  it('Renders error message when sending message to existing user outside of XMTP network', () => {
-    checkElement('message-to-input').type('invalidUser').click();
-    cy.get(`[data-testid=message-to-subtext]`, { timeout: 10000 }).should(
-      'have.text',
-      'Please enter a valid wallet address'
-    );
-    checkMissingElement('message-input');
-  });
-});
+    it('Renders error message when sending message to existing user outside of XMTP network', () => {
+      checkElement('message-to-input').type('invalidUser').click();
+      cy.get(`[data-testid=message-to-subtext]`, { timeout: 10000 }).should(
+        'have.text',
+        'Please enter a valid wallet address'
+      );
+      checkMissingElement('message-input');
+    });
+  }
+);
