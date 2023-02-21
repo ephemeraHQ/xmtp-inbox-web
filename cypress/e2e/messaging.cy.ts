3aa46f255c125521b7de6c89169e504cd1d2f728
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-02-21 22:55:06 +0530

    added test for dm route

--- cypress/e2e/messaging.cy.ts
+++ cypress/e2e/messaging.cy.ts
@@ -23,6 +23,8 @@ describe(
     const testUserWithXmtpAccount =
       "0x78BfD39428C32Be149892d64bEE6C6f90aedEec1";
 
+    const hiXtmpEthAddress = "0x194c31cAe1418D5256E8c58e0d08Aee1046C6Ed0";
+
     const shortMessage = "hello";
     const longMessage =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et ultrices neque ornare aenean euismod elementum. Etiam tempor orci eu lobortis elementum nibh. Tincidunt ornare massa eget egestas purus. Purus sit amet volutpat consequat mauris. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Neque ornare aenean euismod elementum nisi quis eleifend. Amet consectetur adipiscing elit pellentesque habitant morbi. Malesuada fames ac turpis egestas sed tempus urna. Morbi quis commodo odio aenean sed adipiscing diam donec. Id aliquet risus feugiat in. Massa sed elementum tempus egestas sed. Lectus mauris ultrices eros in cursus turpis massa tincidunt. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Cursus eget nunc scelerisque viverra mauris in aliquam. Enim sed faucibus turpis in eu mi bibendum neque egestas. Lectus urna duis convallis convallis tellus id. Vitae semper quis lectus nulla at.";
@@ -59,6 +61,14 @@ describe(
         "Please enter a valid wallet address",
       );
       checkMissingElement("message-input");
+    });
+
+    it("Renders recipient control with correct wallet address", () => {
+      cy.visit("http://localhost:3000/dm/hi.xmtp.eth");
+      checkElement("recipient-wallet-address").should(
+        "have.text",
+        hiXtmpEthAddress,
+      );
     });
   },
 );
