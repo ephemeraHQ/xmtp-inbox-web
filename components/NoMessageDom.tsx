480547d6f753442487ef026c6a111dd7947e804d
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-02-27 18:16:07 +0530

    no message component

---
+++ components/NoMessageDom.tsx
@@ -0,0 +1,29 @@
+import { ArrowCircleRightIcon } from "@heroicons/react/outline";
+import React from "react";
+import { Button } from "../component-library/Button";
+
+const NoMessageDom = () => {
+  return (
+    <div className="h-full flex flex-col items-center text-center justify-center">
+      <div>
+        <img
+          className="h-full w-auto px-16"
+          src="/no-msg.png"
+          alt="No Messages"
+        />
+      </div>
+      <div className="text-lg font-bold mt-8">{"You've got no messages"}</div>
+      <div className="text-sm mt-1 mb-2">
+        Start a conversation to get going!
+      </div>
+      <Button
+        buttonSize="small"
+        category="text"
+        label="Compose your first message"
+        icon={<ArrowCircleRightIcon width={20} />}
+      />
+    </div>
+  );
+};
+
+export default NoMessageDom;
