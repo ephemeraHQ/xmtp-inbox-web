de8b50310b8abec928c0f4d2cc324d1197244bc0
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-04-03 22:53:59 +0400

    minor fix

--- component-library/components/AddressInput/AddressInput.tsx
+++ component-library/components/AddressInput/AddressInput.tsx
@@ -96,7 +96,7 @@ export const AddressInput = ({
               autoComplete="off"
               autoCorrect="false"
               autoCapitalize="off"
-              autoFocus={true}
+              autoFocus
               onChange={(e) =>
                 onChange && onChange((e.target as HTMLInputElement).value)
               }
