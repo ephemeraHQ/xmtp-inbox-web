c14823ff9491f231f9dba2424c4f6db6b761a3a2
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-04-09 17:43:33 +0400

    added check to only track xmtp labs team wallets

--- hooks/useSendMessage.ts
+++ hooks/useSendMessage.ts
@@ -1,6 +1,7 @@
 import { useCallback } from "react";
 import ReactGA from "react-ga4";
 import { useXmtpStore } from "../store/xmtp";
+import { TRACK_WALLETS } from "../helpers";
 
 const useSendMessage = (conversationId: string) => {
   const conversations = useXmtpStore((state) => state.conversations);
@@ -9,11 +10,13 @@ const useSendMessage = (conversationId: string) => {
   const sendMessage = useCallback(
     async (message: string) => {
       await selectedConversation?.send(message);
-      ReactGA.event({
-        category: "Msg sent event",
-        // @ts-expect-error: Property 'client' does not exist on type 'Conversation'
-        action: `${selectedConversation?.client.address}-${conversationId}`,
-      });
+      if (TRACK_WALLETS.includes(conversationId)) {
+        ReactGA.event({
+          category: "Msg sent event",
+          // @ts-expect-error: Property 'client' does not exist on type 'Conversation'
+          action: `${selectedConversation?.client.address}-${conversationId}`,
+        });
+      }
     },
     [selectedConversation],
   );
