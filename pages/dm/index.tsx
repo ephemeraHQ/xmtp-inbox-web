401d05b68c457724524fe78973756ac035ffa572
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-04-25 17:02:34 +0400

    resolved review comment

--- pages/dm/index.tsx
+++ pages/dm/index.tsx
@@ -1,6 +1,16 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Conversation: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    let recipient = window.location.pathname.split("/")[2];
+    if (!recipient) {
+      router.push("/");
+    }
+  }, [window.location.pathname]);
   return <div />;
 };
 
