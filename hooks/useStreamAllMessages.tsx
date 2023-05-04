74992a1125713a598fa030fdecdbf731a647736f
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-05-04 14:25:01 +0400

    reverted the useStreamAllMessages hook changes

--- hooks/useStreamAllMessages.tsx
+++ hooks/useStreamAllMessages.tsx
@@ -5,60 +5,83 @@ import { useXmtpStore } from "../store/xmtp";
 import { fetchEnsName } from "@wagmi/core";
 import { useAccount } from "wagmi";
 import { address } from "../pages/inbox";
-import { useStreamAllMessages as useStreamAllMessagesFromSdk } from "@xmtp/react-sdk";
+import { useClient } from "@xmtp/react-sdk";
 
 let latestMsgId: string;
 
 export const useStreamAllMessages = () => {
   const { address: walletAddress } = useAccount();
 
+  const { client } = useClient();
+
   const convoMessages = useXmtpStore((state) => state.convoMessages);
   const addMessages = useXmtpStore((state) => state.addMessages);
   const setPreviewMessage = useXmtpStore((state) => state.setPreviewMessage);
   const [browserVisible, setBrowserVisible] = useState<boolean>(true);
-
   useEffect(() => {
     window.addEventListener("focus", () => setBrowserVisible(true));
     window.addEventListener("blur", () => setBrowserVisible(false));
   }, []);
 
-  const onMessage = async (message: DecodedMessage) => {
-    const key = getConversationId(message.conversation);
-    setPreviewMessage(key, message);
-
-    const numAdded = addMessages(key, [message]);
-    if (numAdded > 0) {
-      const newMessages = convoMessages.get(key) ?? [];
-      newMessages.push(message);
-      // Below code is to remove duplicate messages from the
-      // newMessages array
-      const uniqueMessages = [
-        ...Array.from(
-          new Map(newMessages.map((item) => [item["id"], item])).values(),
-        ),
-      ];
-      convoMessages.set(key, uniqueMessages);
-      if (
-        latestMsgId !== message.id &&
-        Notification.permission === "granted" &&
-        message.senderAddress !== walletAddress &&
-        !browserVisible
-      ) {
-        const name = await fetchEnsName({
-          address: message.senderAddress as address,
-        });
-        new Notification("XMTP", {
-          body: `${
-            name || shortAddress(message.senderAddress ?? "")
-          }\n${truncate(message.content, 75)}`,
-        });
-
-        latestMsgId = message.id;
-      }
+  useEffect(() => {
+    if (!client) {
+      return;
     }
-  };
 
-  useStreamAllMessagesFromSdk(onMessage);
+    let messageStream: AsyncGenerator<DecodedMessage>;
+
+    const streamAllMessages = async () => {
+      messageStream = await client?.conversations?.streamAllMessages();
+
+      for await (const message of messageStream) {
+        const key = getConversationId(message.conversation);
+        setPreviewMessage(key, message);
+
+        const numAdded = addMessages(key, [message]);
+        if (numAdded > 0) {
+          const newMessages = convoMessages.get(key) ?? [];
+          newMessages.push(message);
+          // Below code is to remove duplicate messages from the
+          // newMessages array
+          const uniqueMessages = [
+            ...Array.from(
+              new Map(newMessages.map((item) => [item["id"], item])).values(),
+            ),
+          ];
+          convoMessages.set(key, uniqueMessages);
+          if (
+            latestMsgId !== message.id &&
+            Notification.permission === "granted" &&
+            message.senderAddress !== walletAddress &&
+            !browserVisible
+          ) {
+            const name = await fetchEnsName({
+              address: message.senderAddress as address,
+            });
+            new Notification("XMTP", {
+              body: `${
+                name || shortAddress(message.senderAddress ?? "")
+              }\n${truncate(message.content, 75)}`,
+            });
+
+            latestMsgId = message.id;
+          }
+        }
+      }
+    };
+
+    const closeMessageStream = async () => {
+      if (messageStream) {
+        await messageStream.return(undefined);
+      }
+    };
+
+    streamAllMessages();
+
+    return () => {
+      closeMessageStream();
+    };
+  }, [client, walletAddress]);
 };
 
 export default useStreamAllMessages;
