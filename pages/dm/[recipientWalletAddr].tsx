c5ad24695c447b7d78a3e9db1cffc4c9c846e5d1
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-05-04 09:11:02 +0400

    changes to make the dm route work

--- pages/dm/[recipientWalletAddr].tsx
+++ pages/dm/[recipientWalletAddr].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { isEnsAddress, isValidRecipientAddressFormat } from "../../helpers";
@@ -7,34 +7,39 @@ import { fetchEnsAddress } from "@wagmi/core";
 
 const DmPage: NextPage = () => {
   const router = useRouter();
+
   const setConversationId = useXmtpStore((state) => state.setConversationId);
   const setRecipientWalletAddress = useXmtpStore(
     (state) => state.setRecipientWalletAddress,
   );
 
-  const routeToInbox = async () => {
-    let recipient = router.query.recipientWalletAddr
-      ? (router.query.recipientWalletAddr as string)
-      : window.location.pathname.split("/").slice(-1)[0];
-    if (isValidRecipientAddressFormat(recipient)) {
-      if (isEnsAddress(recipient)) {
-        recipient =
-          (await fetchEnsAddress({
-            name: recipient,
-          })) ?? "";
-      }
-      setConversationId(recipient);
-      setRecipientWalletAddress(recipient);
-    }
-    router.push("/inbox");
-  };
+  const [recipientWalletAddr, setRecipientWalletAddr] = useState<string>();
 
-  if (
-    window.location.pathname.includes("/dm") ||
-    router.query.recipientWalletAddr
-  ) {
+  useEffect(() => {
+    if (router.query.recipientWalletAddr)
+      setRecipientWalletAddr(router.query.recipientWalletAddr as string);
+  }, [router.query.recipientWalletAddr]);
+
+  useEffect(() => {
+    const routeToInbox = async () => {
+      if (recipientWalletAddr) {
+        let recipient = recipientWalletAddr as string;
+        if (isValidRecipientAddressFormat(recipient)) {
+          if (isEnsAddress(recipient)) {
+            recipient =
+              (await fetchEnsAddress({
+                name: recipient,
+              })) ?? "";
+          }
+          setConversationId(recipient);
+          setRecipientWalletAddress(recipient);
+        }
+      }
+      router.push("/inbox");
+    };
+
     routeToInbox();
-  }
+  }, [window.location.pathname, recipientWalletAddr]);
 
   return <div></div>;
 };
