02169301b30ef206588211a4f507eca7688b6a80
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-05-08 13:49:23 +0530

    removed incorrect test

--- cypress/e2e/connect_disconnect.cy.ts
+++ cypress/e2e/connect_disconnect.cy.ts
@@ -58,27 +58,6 @@ describe(
       cy.get(`[data-testid="disconnect-wallet-cta"]`).click();
     });
 
-    // Need to fix this
-    // it("Opens feedback message conversation and gets the right message", () => {
-    //   checkElement("conversations-list-panel");
-    //   // eslint-disable-next-line cypress/no-unnecessary-waiting
-    //   cy.wait(500);
-    //   cy.get(`[data-testid=message-tile-text]`, { timeout: TIMEOUT })
-    //     .first()
-    //     .click();
-    //   // eslint-disable-next-line cypress/no-unnecessary-waiting
-    //   cy.wait(15000);
-    //   // Need to break up the click chain for GitHub actions
-    //   checkElement("new-message-icon-cta");
-    //   cy.get(`[data-testid=new-message-icon-cta]`).click({ timeout: TIMEOUT });
-    //   checkElement("message-to-input").type(XMTP_FEEDBACK_ADDRESS);
-
-    //   cy.get(`[data-testid=message-tile-text]`)
-    //     .children("span")
-    //     .first()
-    //     .should("have.text", "Welcome to XMTP Inbox 👋");
-    // });
-
     it("Opens new message view when clicking on plus icon from left panel", () => {
       // Need to break up the click chain for GitHub actions
       cy.get(`[data-testid=new-message-icon-cta]`).click();
