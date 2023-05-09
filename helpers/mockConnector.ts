e903b8e5c8a8b78cf31961ab957e3f304f493ae1
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-05-09 11:41:56 +0530

    fix mock connector being called multiple times

--- helpers/mockConnector.ts
+++ helpers/mockConnector.ts
@@ -5,9 +5,18 @@ import { isAppEnvDemo } from "./env";
 // mock connector for demos
 export const getMockConnector = () => {
   if (typeof window !== "undefined" && isAppEnvDemo()) {
-    const mockWallet = Wallet.createRandom();
-    return new MockConnector({
-      options: { signer: mockWallet },
-    });
+    const storedMockConnector = JSON.parse(
+      sessionStorage.getItem("mockConnector") ?? "{}",
+    );
+    if (!storedMockConnector.connect) {
+      const mockWallet = Wallet.createRandom();
+      const mockConnector = new MockConnector({
+        options: { signer: mockWallet },
+      });
+      sessionStorage.setItem("mockConnector", JSON.stringify(mockConnector));
+      return mockConnector;
+    } else {
+      return storedMockConnector;
+    }
   }
 };
