00bfd2d6f864c74ac0749a7b23e1e46d74f2ed30
daria-github <daria@xmtp.com>
2023-05-25 11:19:06 -0700

    updated types for MessageContentWrapper

--- wrappers/MessageContentWrapper.tsx
+++ wrappers/MessageContentWrapper.tsx
@@ -2,16 +2,39 @@ import { Interweave } from "interweave";
 import { EmailMatcher, UrlMatcher } from "interweave-autolink";
 import { EmojiMatcher, useEmojiData } from "interweave-emoji";
 import type { MouseEvent } from "react";
+import React from "react";
+import RemoteAttachmentMessageTile from "../component-library/components/RemoteAttachmentMessageTile/RemoteAttachmentMessageTile";
+import { RemoteAttachment } from "../classes/RemoteAttachment";
 
-const MessageContentWrapper = ({ content }: { content: string }) => {
+interface MessageContentWrapperProps {
+  content: string | RemoteAttachment;
+  isSelf: boolean;
+  isLoading: boolean;
+  isError: boolean;
+}
+
+const MessageContentWrapper = ({
+  content,
+  isSelf,
+  isLoading,
+  isError,
+}: MessageContentWrapperProps) => {
   const [, source] = useEmojiData({
     compact: false,
     shortcodes: ["emojibase"],
   });
-  return (
+
+  return typeof content !== "string" && content.url ? (
+    <RemoteAttachmentMessageTile
+      content={content}
+      isSelf={isSelf}
+      isLoading={isLoading}
+      isError={isError}
+    />
+  ) : (
     <span className="interweave-content" data-testid="message-tile-text">
       <Interweave
-        content={content}
+        content={content as string}
         newWindow
         escapeHtml
         onClick={(event: MouseEvent<HTMLDivElement>) => event.stopPropagation()}
