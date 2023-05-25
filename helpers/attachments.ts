9ed7b090222f81395809d3aa36b942a2889fe28d
daria-github <daria@xmtp.com>
2023-05-25 14:50:14 -0700

    updated tests

--- helpers/attachments.ts
+++ helpers/attachments.ts
@@ -1,4 +1,16 @@
+/**
+ * Returns a human readable file size string.
+ *
+ * @param bytes - The number of bytes.
+ * @param si - Use SI units (KB, MB, GB) instead of binary (KiB, MiB, GiB). Default is false.
+ * @param dp - Number of decimal places to display. Default is 1.
+ * @returns The human readable file size string.
+ */
 export const humanFileSize = (bytes: number, si = false, dp = 1) => {
+  // Throws error if > 100 MB
+  if (bytes > 100000000) {
+    throw new Error("File too large!");
+  }
   const thresh = si ? 1000 : 1024;
 
   if (Math.abs(bytes) < thresh) {
