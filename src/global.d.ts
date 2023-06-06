bd7c132afafcff21e10c05199e91e76c01d5d6e5
daria-github <daria@xmtp.com>
2023-06-06 13:08:01 -0700

    move global types out of folder

---
+++ src/global.d.ts
@@ -0,0 +1,30 @@
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: any;
+  export default content;
+}
+
+declare module "react-blockies" {
+  import React from "react";
+  interface BlockiesProps {
+    seed: string;
+    size?: number;
+    scale?: number;
+    color?: string;
+    bgColor?: string;
+    spotColor?: string;
+    className?: string;
+  }
+  const Blockies: React.FC<BlockiesProps>;
+
+  export default Blockies;
+}
