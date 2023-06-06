8b030899dc86e0a0a9fb8ffb582063fe364145e4
daria-github <daria@xmtp.com>
2023-06-06 13:17:38 -0700

    remove ga tags

--- src/hooks/useSendMessage.ts
+++ src/hooks/useSendMessage.ts
@@ -2,7 +2,6 @@ import { useCallback } from "react";
 import { getConversationId, isValidLongWalletAddress } from "../helpers";
 import { useXmtpStore } from "../store/xmtp";
 import { address } from "../pages/inbox";
-import { emitMsgSentEvent } from "../helpers/internalTracking";
 import {
   useSendMessage as useSendMessageHook,
   useStartConversation,
@@ -116,18 +115,6 @@ const useSendMessage = (conversationId: address, attachment?: Attachment) => {
         } else {
           await sendMessageFromHook(message as string);
         }
-
-        /* The emitMsgSentEvent function is called only when
-            specific XMTP Labs team wallets use
-            the internal domain alpha.xmtp.chat. This
-            tracking is temporary and meant to help
-            surface insights about team usage to
-            help build a better app. */
-        await emitMsgSentEvent(
-          // @ts-expect-error: Property 'client' does not exist on type 'Conversation'
-          selectedConversation?.client.address,
-          recipientWalletAddress as address,
-        );
       }
     },
     [conversationId, recipientWalletAddress, conversations, attachment],
