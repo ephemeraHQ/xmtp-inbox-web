d085e6df8bcb8b571718b220dd539556907daba4
Ry Racherbaumer <ry@xmtp.com>
2023-10-13 07:44:28 -0500

    WIP

--- src/controllers/MessageContentController.tsx
+++ src/controllers/MessageContentController.tsx
@@ -3,6 +3,7 @@ import {
   ContentTypeId,
   ContentTypeText,
   getReadReceipt,
+  useConversation,
   useSendMessage,
 } from "@xmtp/react-sdk";
 import type { CachedConversation, CachedMessage } from "@xmtp/react-sdk";
@@ -25,6 +26,7 @@ const sendReadReceipt = debounce(
     conversation: CachedConversation,
     sendMessage: ReturnType<typeof useSendMessage>["sendMessage"],
   ) => {
+    console.log("actually sending read receipt...");
     await sendMessage(conversation, {}, ContentTypeReadReceipt);
   },
   1000,
@@ -36,32 +38,41 @@ const MessageContentController = ({
   isSelf,
 }: MessageContentControllerProps) => {
   const { sendMessage } = useSendMessage();
+  const { getCachedByTopic } = useConversation();
   const contentType = ContentTypeId.fromString(message.contentType);
 
   // when message content is rendered, update the conversation's read receipt
   useEffect(() => {
-    const readReceipt = getReadReceipt(conversation);
-    console.log("isSelf", isSelf);
-    console.log("readReceipt", readReceipt);
-    console.log("message.sentAt", message.sentAt);
-    if (readReceipt) {
-      console.log(
-        "incoming message sent after read receipt",
-        isAfter(message.sentAt, readReceipt),
-      );
-    }
+    const maybeSendReadReceipt = async () => {
+      // ensure latest conversation metadata
+      const updatedConversation = await getCachedByTopic(conversation.topic);
+      if (updatedConversation) {
+        const readReceipt = getReadReceipt(updatedConversation);
+        console.log("isSelf", isSelf);
+        console.log("readReceipt", readReceipt);
+        console.log("message.sentAt", message.sentAt);
+        if (readReceipt) {
+          console.log(
+            "incoming message sent after read receipt",
+            isAfter(message.sentAt, readReceipt),
+          );
+        }
 
-    // if the message was sent by someone else, and
-    // there's no read receipt for this conversation, or
-    // there's a read receipt and the message comes after it
-    if (
-      !isSelf &&
-      (!readReceipt || (readReceipt && isAfter(message.sentAt, readReceipt)))
-    ) {
-      console.log("sending read receipt...");
-      // send a read receipt message
-      void sendReadReceipt(conversation, sendMessage);
-    }
+        // if the message was sent by someone else, and
+        // there's no read receipt for this conversation, or
+        // there's a read receipt and the message comes after it
+        if (
+          !isSelf &&
+          (!readReceipt ||
+            (readReceipt && isAfter(message.sentAt, readReceipt)))
+        ) {
+          console.log("attempting to send read receipt...");
+          // send a read receipt message
+          void sendReadReceipt(conversation, sendMessage);
+        }
+      }
+    };
+    void maybeSendReadReceipt();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
