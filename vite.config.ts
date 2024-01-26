0a8fa5c2fcb58d942e1ccd4320e5f627eaf68aef
daria-github <daria@xmtp.com>
2024-01-26 15:19:01 -0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
+  optimizeDeps: {
+    exclude: ["@xmtp/user-preferences-bindings-wasm"],
+  },
 });
