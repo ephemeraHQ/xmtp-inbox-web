c5c2f81163e568f9507336de2bf324cca5af9785
daria-github <daria@xmtp.com>
2024-02-02 17:02:14 -0800

    updated frames get/post logic

--- src/controllers/FullMessageController.tsx
+++ src/controllers/FullMessageController.tsx
@@ -1,9 +1,16 @@
 import type { CachedConversation, CachedMessageWithId } from "@xmtp/react-sdk";
-import { useClient } from "@xmtp/react-sdk";
+import { Client, useClient } from "@xmtp/react-sdk";
+import { FramesClient } from "@xmtp/frames-client";
+import { useEffect, useState } from "react";
+import type { PrivateKeyAccount, Transport, WalletClient } from "viem";
+import type { mainnet } from "wagmi";
+import { useWalletClient } from "wagmi";
 import { FullMessage } from "../component-library/components/FullMessage/FullMessage";
-import { shortAddress } from "../helpers";
+import { classNames, shortAddress } from "../helpers";
 import MessageContentController from "./MessageContentController";
 import { useXmtpStore } from "../store/xmtp";
+import { Frame } from "../component-library/components/Frame/Frame";
+import { getFrameInfo } from "../helpers/getFrameInfo";
 
 interface FullMessageControllerProps {
   message: CachedMessageWithId;
@@ -11,29 +18,116 @@ interface FullMessageControllerProps {
   isReply?: boolean;
 }
 
+export type FrameInfo = {
+  image: string;
+  title: string;
+  buttons: string[];
+  postUrl: string;
+};
+
 export const FullMessageController = ({
   message,
   conversation,
   isReply,
 }: FullMessageControllerProps) => {
   const { client } = useClient();
+  const { data: walletClient } = useWalletClient();
+
+  const conversationTopic = useXmtpStore((state) => state.conversationTopic);
+
+  const [frameInfo, setFrameInfo] = useState<FrameInfo | undefined>(undefined);
+  const [frameButtonUpdating, setFrameButtonUpdating] = useState<number>(0);
+
+  const handleFrameButtonClick = async (buttonNumber: number) => {
+    if (!frameInfo) {
+      return;
+    }
+    const url = frameInfo.image;
+    const messageId = String(message.id);
+
+    setFrameButtonUpdating(buttonNumber);
+
+    const xmtpClient = await Client.create(
+      walletClient as WalletClient<
+        Transport,
+        typeof mainnet,
+        PrivateKeyAccount
+      >,
+    );
+    const framesClient = new FramesClient(xmtpClient);
+
+    const payload = await framesClient.signFrameAction(
+      url,
+      buttonNumber,
+      conversationTopic as string,
+      messageId,
+    );
+    const updatedFrameMetadata = await FramesClient.postToFrame(
+      frameInfo.postUrl,
+      payload,
+    );
+    const updatedFrameInfo = getFrameInfo(updatedFrameMetadata.extractedTags);
+
+    setFrameInfo(updatedFrameInfo);
+    setFrameButtonUpdating(0);
+  };
+
+  useEffect(() => {
+    if (typeof message.content === "string") {
+      const words = message.content?.split(/(\r?\n|\s+)/);
+      const urlRegex =
+        /^(http[s]?:\/\/)?([a-z0-9.-]+\.[a-z0-9]{1,}\/.*|[a-z0-9.-]+\.[a-z0-9]{1,})$/i;
+
+      void Promise.all(
+        words.map(async (word) => {
+          const isUrl = !!word.match(urlRegex)?.[0];
+
+          if (isUrl) {
+            const metadata = await FramesClient.readMetadata(word);
+            if (metadata) {
+              const info = getFrameInfo(metadata.extractedTags);
+              setFrameInfo(info);
+            }
+          }
+        }),
+      );
+    }
+  }, [message?.content]);
+
   const recipientName = useXmtpStore((s) => s.recipientName);
+  const alignmentStyles =
+    client?.address === message.senderAddress
+      ? "items-end justify-end"
+      : "items-start justify-start";
 
   return (
-    <FullMessage
-      isReply={isReply}
-      message={message}
-      conversation={conversation}
-      key={message.xmtpID}
-      from={{
-        displayAddress: recipientName ?? shortAddress(message.senderAddress),
-        isSelf: client?.address === message.senderAddress,
-      }}
-      datetime={message.sentAt}>
-      <MessageContentController
+    <div
+      className={classNames(
+        "flex flex-col w-full px-4 md:px-8",
+        alignmentStyles,
+      )}>
+      <FullMessage
+        isReply={isReply}
         message={message}
-        isSelf={client?.address === message.senderAddress}
-      />
-    </FullMessage>
+        conversation={conversation}
+        key={message.xmtpID}
+        from={{
+          displayAddress: recipientName ?? shortAddress(message.senderAddress),
+          isSelf: client?.address === message.senderAddress,
+        }}
+        datetime={message.sentAt}>
+        <MessageContentController
+          message={message}
+          isSelf={client?.address === message.senderAddress}
+        />
+      </FullMessage>
+      {frameInfo && (
+        <Frame
+          info={frameInfo}
+          handleClick={handleFrameButtonClick}
+          frameButtonUpdating={frameButtonUpdating}
+        />
+      )}
+    </div>
   );
 };
