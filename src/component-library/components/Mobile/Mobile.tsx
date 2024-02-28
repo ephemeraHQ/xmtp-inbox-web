8a7f50216fcf6c6d24fc69357ec6b2c5334f5623
daria-github <daria@xmtp.com>
2024-02-28 11:34:15 -0800

    design fixes

--- src/component-library/components/Mobile/Mobile.tsx
+++ src/component-library/components/Mobile/Mobile.tsx
@@ -1,24 +1,24 @@
-import { DeviceMobileIcon } from "@heroicons/react/solid";
+import xmtpIcon from "../../../../public/xmtp-icon.png";
 
 const LinkEle = ({ url, text }: { url: string; text: string }) => (
   <a
     href={url}
     target="_blank"
-    className="text-blue-600 m-1 text-center cursor-pointer"
+    className="text-indigo-600 m-1 pb-4 cursor-pointer font-bold"
     rel="noreferrer">
     {text}
   </a>
 );
 
 export const Mobile = () => (
-  <div className="flex flex-col items-start justify-center h-screen p-8 text-left">
+  <div className="flex flex-col items-center justify-center h-screen p-8 text-center">
     <div className="flex flex-col w-full items-center">
-      <DeviceMobileIcon width="100" className="text-indigo-600" />
-      <h1 className="text-2xl font-bold">Looks like you&apos;re on mobile!</h1>
+      <img src={xmtpIcon} alt="XMTP logo" className="h-24 w-24 mb-4" />
+      <h1 className="text-3xl font-bold">Looks like you&apos;re on mobile!</h1>
     </div>
-    <p className="text-left mt-8">For mobile-friendly XMTP chat:</p>
-    <ul className="list-disc m-4">
-      <li className="ml-1">
+    <p className="text-left mt-4 font-bold">For mobile-friendly chat:</p>
+    <ul>
+      <li className="mt-4">
         Try group chat on the dev network in Converse Preview:
       </li>
       <LinkEle url="https://testflight.apple.com/join/xEJOvzEx" text="iOS" />
@@ -27,12 +27,12 @@ export const Mobile = () => (
         url="https://drive.google.com/file/d/1rUtCmtIB6VzHNW8PDJ1TMBRuI2OEOdcg/view?usp=drive_link"
         text="Android"
       />
-      <li className="ml-1">
+      <li className="mt-4">
         Try subscription notifications and 1:1 chats in
         <LinkEle url="https://go.cb-w.com" text="Coinbase Wallet" />
       </li>
-      <li className="ml-1">
-        Devs: Build on the open source
+      <li className="mt-4">
+        Devs, build on the open source
         <LinkEle
           url="https://github.com/xmtp-labs/xmtp-inbox-mobile"
           text="XMTP React Native Reference App"
